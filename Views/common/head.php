9b535a4c6378db6089e7550326638a17b54b2038
KoheyNishita <5024kohy.nishi@gmail.com>
2021-11-15 11:51:06 +0900

    プロフ編集・フォロー機能実装

--- Views/common/head.php
+++ Views/common/head.php
@@ -12,5 +12,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous" defer></script>
 <!-- いいね！ボタンのJS -->
 <script src="<?php echo HOME_URL; ?>Views/js/likes.js" defer></script>
+<!-- フォローボタンのJS -->
+<script src="<?php echo HOME_URL; ?>Views/js/follows.js" defer></script>
 <!-- JSのファイルにdefer属性を指定すると、JSの読み込みを遅らせる→HTML全体の読み込みが優先され、ページが早く読み込まれる -->
 <!-- 通常読み込まれるScriptが、deferありのScriptに依存しているとエラーが起こる場合アリ -->
